f99f193b9f7b657d078f735d951cb596d192db2d
bankoksu <seshomarualex@gmail.com>
2018-02-19 13:52:32 +0100

    pruebas del primer apartado

---
+++ pigcoin/src/main/java/org/mvpigs/pigcoins/Wallet.java
@@ -0,0 +1,64 @@
+package org.mvpigs.pigcoins;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Wallet {
+    private PublicKey address;
+    private PrivateKey SK;
+    private double totalInput = 0.0d;
+    private double totalOutput = 0.0d;
+    private double balance = 0.0d;
+
+    public void setAddress(PublicKey address) {
+        this.address = address;
+    }
+
+    public PublicKey getAddress() {
+        return this.address;
+    }
+
+    public void setSK(PrivateKey SK) {
+        this.SK = SK;
+    }
+
+    public PrivateKey getSK(){
+        return this.SK;
+    }
+
+    public void setTotalInput(double input) {
+        this.totalInput += input;
+    }
+
+    public void setTotalOutput(double output) {
+        this.totalOutput += output;
+    }
+
+    public void generateKeyPair(){
+        KeyPair pair = GenSig.generateKeyPair();
+        setAddress(pair.getPublic());
+        setSK(pair.getPrivate());
+    }
+
+    public double getTotalInput() {
+        return this.totalInput;
+    }
+
+    public double getTotalOutput() {
+        return this.totalOutput;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance() {
+        double balance =  getTotalInput() - getTotalOutput();
+        if (balance >= 0) {
+         this.balance = balance;
+        }
+    }
---
+++ pigcoin/src/test/java/org/mvpigs/pigcoins/WalletTest.java
@@ -0,0 +1,34 @@
+package org.mvpigs.pigcoins;
+
+import java.util.Map;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import java.security.KeyPair;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WalletTest{
+    Wallet wallet_1 = null;
+    Wallet wallet_2 = null;
+    @Before
+    public void SetUp() {
+        wallet_1 = new Wallet();
+        wallet_1.generateKeyPair();
+
+        wallet_2 = new Wallet();
+        wallet_2.generateKeyPair();
+    }
+
+    @Test
+    public void testComprobarWallet (){
+        Wallet wallet = new Wallet();
+
+        KeyPair pair = GenSig.generateKeyPair();
+
+        wallet.setSK(pair.getPrivate());
+        wallet.setAddress(pair.getPublic());
+
+        assertNotNull(wallet);
+        assertEquals(pair.getPublic(), wallet.getAddress());
+
+    }
